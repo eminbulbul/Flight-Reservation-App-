002a9c4b22babc354770df85b8730b1bcf1608c7
m_emin <mehmeteminbul@gmail.com>
2022-06-24 22:33:32 +0300

    serializers and permissions added

--- flight/serializers.py
+++ flight/serializers.py
@@ -15,3 +15,29 @@ class FlightSerializer(serializers.ModelSerializer):
             "date_of_departure",
             "etd"
         )
+        
+        
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = '__all__'
+        
+        
+class ReservationSerializer(serializers.ModelSerializer):
+    
+    passenger = PassengerSerializer(many=True, required=False)
+    flight = serializers.StringRelatedField()  # default read_only=True
+    user = serializers.StringRelatedField()  # default read_only=True
+    flight_id = serializers.IntegerField(write_only=True)
+    user_id = serializers.IntegerField(write_only=True, required=False)
+    class Meta:
+        model = Reservation
+        fields =(
+            "id",
+            "flight",
+            "flight_id",
+            "user",
+            "user_id",
+            "passenger"
+        )
+        
